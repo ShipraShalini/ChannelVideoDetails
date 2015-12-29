1b5212cd06e9cfd82f129a73ee674ff7916a6986
Shipra <code.shipra@gmail.com>
2015-12-29 17:07:05 +0530

    creates youtube api functions

--- video/urls.py
+++ video/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from src.api.views.videoview import *
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+        url(r'^admin/', admin.site.urls),
+        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+        url(r'^list/', listview.as_view()),
+        url(r'^detail/', detailsview.as_view()),
+         ]
